b4a6e924963bd9c0bc5c09335d28ea0193d96d52
Patrick Hogan <patrick@callinize.com>
2015-03-29 04:58:48 -0500

    Merge remote-tracking branch 'remotes/upstream/master'
    Conflicts:
    	lib/Chrome.js
    	lib/chrome/I18n.js
    
    Improve test coverage
    Add new messages.json
    Add to readme

--- test/unit/lib/chrome/Tabs.spec.js
+++ test/unit/lib/chrome/Tabs.spec.js
@@ -1,10 +1,10 @@
 var Tabs = hmt.lib('chrome', 'Tabs');
 
 describe('chrome.tabs', function () {
-  var tab;
+  var api;
 
   beforeEach(function () {
-    tab = new Tabs();
+    api = new Tabs();
   });
 
   describe('get', function () {
@@ -12,24 +12,120 @@ describe('chrome.tabs', function () {
 
     describe('with callback', function () {
       beforeEach(function () {
-        tab.create({ name: 'foo' }, cb);
+        api.get({ name: 'foo' }, cb);
       });
 
       it('should provide spy functionality', function () {
-        hmt.assert.equal(tab.create.callCount, 1);
+        hmt.assert.equal(api.get.callCount, 1);
       });
     });
 
     describe('without callback', function () {
       beforeEach(function () {
-        tab.create({ name: 'foo' });
+        api.get({ name: 'foo' });
       });
 
       it('should provide spy functionality', function () {
-        hmt.assert.equal(tab.create.callCount, 1);
+        hmt.assert.equal(api.get.callCount, 1);
+      });
+    });
+  });
+
+  describe('getCurrent', function () {
+    var cb = hmt.spy();
+
+    describe('with callback', function () {
+      beforeEach(function () {
+        api.getCurrent({ name: 'foo' }, cb);
       });
 
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.getCurrent.callCount, 1);
+      });
     });
 
+    describe('without callback', function () {
+      beforeEach(function () {
+        api.getCurrent({ name: 'foo' });
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.getCurrent.callCount, 1);
+      });
+    });
+  });
+
+  describe('create', function () {
+    var cb = hmt.spy();
+
+    describe('with callback', function () {
+      beforeEach(function () {
+        api.create({ name: 'foo' }, cb);
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.create.callCount, 1);
+      });
+    });
+
+    describe('without callback', function () {
+      beforeEach(function () {
+        api.create({ name: 'foo' });
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.create.callCount, 1);
+      });
+    });
+  });
+
+
+  describe('update', function () {
+    var cb = hmt.spy();
+
+    describe('with callback', function () {
+      beforeEach(function () {
+        api.update({ name: 'foo' }, cb);
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.update.callCount, 1);
+      });
+    });
+
+    describe('without callback', function () {
+      beforeEach(function () {
+        api.update({ name: 'foo' });
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.update.callCount, 1);
+      });
+    });
+  });
+
+
+  describe('remove', function () {
+    var cb = hmt.spy();
+
+    describe('with callback', function () {
+      beforeEach(function () {
+        api.remove({ name: 'foo' }, cb);
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.remove.callCount, 1);
+      });
+    });
+
+    describe('without callback', function () {
+      beforeEach(function () {
+        api.remove({ name: 'foo' });
+      });
+
+      it('should provide spy functionality', function () {
+        hmt.assert.equal(api.remove.callCount, 1);
+      });
+    });
   });
 });
