66c2b775056b575f8ddec707e54e62b5754f9862
Seth McLaughlin <s@sethmcl.com>
2015-05-02 15:11:27 -0700

    Disable some unit tests for functionality that is not yet implemented in I18n module

--- test/unit/lib/chrome/i18n.spec.js
+++ test/unit/lib/chrome/i18n.spec.js
@@ -6,16 +6,16 @@ describe('chrome.I18n', function () {
 
   beforeEach(function () {
     api = new I18n();
+    api.loadDefaults(hmt.path('fixtures', 'messages.json'));
   });
 
   describe('getMessage', function(){
     var cb = hmt.spy();
 
     describe('with callback', function () {
-
       it('should provide spy functionality', function () {
         var test = api.getMessage('test');
-        hmt.assert(test, 'test');
+        hmt.assert(test === 'test');
       });
     });
   });
@@ -29,20 +29,21 @@ describe('chrome.I18n', function () {
   });
 
 
-  describe('setLocale', function(){
-    it('should set a local without error', function () {
-      var test = api.setLocale('GB');
-      hmt.assert(!test, 'test without error');
-    });
-  });
+  // describe('setLocale', function(){
+  //   it('should set a local without error', function () {
+  //     var test = api.setLocale('GB');
+  //     debugger;
+  //     hmt.assert(!test, 'test without error');
+  //   });
+  // });
 
-  describe('setMessagesPath', function(){
-    it('should set a setMessagesPath different from default', function () {
-      var dir = path.join(__dirname, '/../../../fixtures/messages.json');
-      api.setMessagesPath(dir);
-      var test = api._locales;
-      hmt.assert(test.fixture.message === 'test', 'test without error');
-    });
-  });
+  // describe('setMessagesPath', function(){
+  //   it('should set a setMessagesPath different from default', function () {
+  //     var dir = path.join(__dirname, '/../../../fixtures/messages.json');
+  //     api.setMessagesPath(dir);
+  //     var test = api._locales;
+  //     hmt.assert(test.fixture.message === 'test', 'test without error');
+  //   });
+  // });
 
 });
